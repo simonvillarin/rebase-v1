ee972680eab4f8faeb0e700535d5a7befae12804
Simon James Villarin <57483212+simonvillarin@users.noreply.github.com>
2026-01-08 20:03:27 +0800

    Revert "revert"
    This reverts commit c2e99fccda90cb8604587630b6cba3b0ebb36a57.

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(4);
         System.out.println(1);
+        System.out.println(2);
+        System.out.println(3);
     }
 }
